89522ef1ca546cc65da382212732708b4926b5c3
8bury <melbrm30032005@gmail.com>
2025-02-24 23:02:01 -0300

    adiciona css na calculadora

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
-import Output from './components/Output';
-import CalculatorGrid from './components/CalculatorGrid';
 import React from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="calculator">
-      <Output />
-      <CalculatorGrid />
+      <div className="output">
+        <div className="previous-operand">3123</div>
+        <div className="current-operand">213</div>
+      </div>
+      <button className='button2'>AC</button>
+      <button className='button'>DEL</button>
+      <button className='button'>+</button>
+      <button className='button'>1</button>
+      <button className='button'>2</button>
+      <button className='button'>3</button>
+      <button className='button'>*</button>
+      <button className='button'>4</button>
+      <button className='button'>5</button>
+      <button className='button'>6</button>
+      <button className='button'>+</button>
+      <button className='button'>7</button>
+      <button className='button'>8</button>
+      <button className='button'>9</button>
+      <button className='button'>-</button>
+      <button className='button'>.</button>
+      <button className='button'>0</button>
+      <button className='button2'>=</button>
     </div>
   )
 }
