51d334b56d16f9b19b1a196d5c070a7f366905b8
8bury <melbrm30032005@gmail.com>
2025-02-26 21:14:47 -0300

    adiciona funcionalidade para o restante dos botoes

--- src/App.js
+++ src/App.js
@@ -11,6 +11,18 @@ export const ACTIONS = {
   EVALUATE: "evaluate"
 }
 
+const calculate = (prev, current, operation) => {
+  const prevNum = parseFloat(prev);
+  const currentNum = parseFloat(current);
+  switch(operation) {
+    case "+": return prevNum + currentNum;
+    case "-": return prevNum - currentNum;
+    case "*": return prevNum * currentNum;
+    case "/": return prevNum / currentNum;
+    default: return "";
+  }
+}
+
 function reducer(state, { type, payload }) {
   switch (type) {
     case ACTIONS.ADD_DIGIT:
@@ -28,11 +40,12 @@ function reducer(state, { type, payload }) {
         }
       }
       if (state.previousOperand !== ""){
-        return {...state, 
-          previousOperand: `${parseFloat(state.currentOperand) + parseFloat(state.previousOperand)}`,
-            currentOperand: ``,
-            operator: payload.operator
-          }
+        return {
+          ...state, 
+          previousOperand: calculate(state.previousOperand, state.currentOperand, state.operator).toString(),
+          currentOperand: "",
+          operator: payload.operator
+        }
       }
       return {
         ...state,
@@ -40,6 +53,36 @@ function reducer(state, { type, payload }) {
         currentOperand: ``,
         operator: payload.operator,
       }
+    case ACTIONS.EVALUATE:
+      if (state.currentOperand === "" || state.previousOperand === ""){
+        return state;
+      }
+      return {
+        ...state,
+        previousOperand: "",
+        currentOperand: calculate(state.previousOperand, state.currentOperand, state.operator).toString(),
+        operator: ""
+      };
+      case ACTIONS.CLEAR:
+        return {
+          previousOperand: "",
+          currentOperand: "",
+          operator: ""
+        }
+      case ACTIONS.REMOVE_DIGIT:
+        if (state.currentOperand === ""){
+            return state
+          }
+        if (state.currentOperand[state.currentOperand.length - 2] === "."){
+            return {
+                ...state,
+                currentOperand: state.currentOperand.slice(0, -2)
+            }
+          }
+          return {
+            ...state,
+            currentOperand: state.currentOperand.slice(0, -1)
+        }
     default:
       console.error("Invalid action type");
       return state;
@@ -58,9 +101,9 @@ function App() {
         <div className="previous-operand">{previousOperand}{operator}</div>
         <div className="current-operand">{currentOperand}</div>
       </div>
-      <button className="button2">AC</button>
-      <button className="button">DEL</button>
-      <OperatorButton operator="+" dispatch={dispatch}/>
+      <button className="button2" onClick={() => dispatch({type: ACTIONS.CLEAR})}>AC</button>
+      <button className="button" onClick={() => dispatch({type: ACTIONS.REMOVE_DIGIT})}>DEL</button>
+      <OperatorButton operator="/" dispatch={dispatch}/>
       <DigitButton digit={"1"} dispatch={dispatch}/>
       <DigitButton digit={2} dispatch={dispatch}/>
       <DigitButton digit={3} dispatch={dispatch}/>
@@ -75,7 +118,7 @@ function App() {
       <OperatorButton operator="-" dispatch={dispatch}/>
       <DigitButton digit="." dispatch={dispatch}/>
       <DigitButton digit={0} dispatch={dispatch}/>
-      <button className="button2">=</button>
+      <button className="button2" onClick={() => dispatch({type: ACTIONS.EVALUATE})}>=</button>
     </div>
   )
 }
